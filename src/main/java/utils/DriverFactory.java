89c87c5abde7104f1312e468dde2d0dd9d6cf754
rubeldm123 <rubeldm123@gmail.com>
2025-01-02 09:54:36 -0600

    Add chrome options

--- src/main/java/utils/DriverFactory.java
+++ src/main/java/utils/DriverFactory.java
@@ -2,15 +2,21 @@ package utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.chrome.ChromeOptions;
 public class DriverFactory {
     private static WebDriver driver;
 
     public static WebDriver getDriver() {
         if (driver == null) {
             System.setProperty("webdriver.chrome.driver", "/Users/mdrubel/Documents/seleniumJavaProject10/Project10/chromedriver");
-            driver = new ChromeDriver();
-            driver.manage().window().maximize();
+            // Set Chrome options for headless mode
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+
+            // Initialize WebDriver
+           driver = new ChromeDriver(options);;
         }
         return driver;
     }
